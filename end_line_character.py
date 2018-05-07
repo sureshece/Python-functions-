338c0ae507f30214f3c76ad9569d9de43391438d
Suresh Nallasivam <suresh22ece@gmail.com>
2018-05-07 11:33:16 +0200

    Update end_line_character.py

--- end_line_character.py
+++ end_line_character.py
@@ -2,10 +2,10 @@ import sys
 import re
 import io
 
-with open('your filename or location', "rb") as fp: #Open and read the fil in read_binary mode
+with open('your filename or location', "rb") as fp: #Open and read the file in read_binary mode
 
     for line in fp: 
-        if b"\r\n" in line:
+        if b"\r\n" in line: # In if statement "b" means that all data read from the file is returned as bytes objects, not str.
             print("CR+LF (Windows) format.")
         elif b"\n" in line:
             print("LF (UNIX) format.")
